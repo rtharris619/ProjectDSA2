70457b6bb73c0c33ca68301f4f109a42c3643ad1
Ryan Harris <ryan@synthesis.co.za>
2025-11-05 08:57:41 +0200

    Breadth first search

--- Algomonster/BreadthFirstSearch/BinaryTreeMinDepth.cs
+++ Algomonster/BreadthFirstSearch/BinaryTreeMinDepth.cs
@@ -9,6 +9,33 @@ public class BinaryTreeMinDepth
         if (root == null)
             return 0;
 
-        return 1;
+        int depth = 0;
+        var q = new Queue<Node<int>>();
+        q.Enqueue(root);
+
+        while (q.Count > 0)
+        {
+            int n = q.Count;
+            for (int i = 0; i < n; i++)
+            {
+                var node = q.Dequeue();
+                if (node.left == null && node.right == null)
+                    return depth;
+                if (node.left != null)
+                    q.Enqueue(node.left);
+                if (node.right != null)
+                    q.Enqueue(node.right);
+            }
+            depth++;
+        }
+
+        return depth;
+    }
+
+    public static void Driver()
+    {
+        Node<int> root = ExtractTree("1 2 4 x 7 x x 5 x x 3 x 6 x x", int.Parse);
+        var result = MinDepth(root);
+        Console.WriteLine(result);
     }
 }
