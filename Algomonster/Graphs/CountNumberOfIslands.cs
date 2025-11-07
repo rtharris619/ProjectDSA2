244e64f11ed20247f88c44b8fac7cf973875c335
Ryan Harris <ryan@synthesis.co.za>
2025-11-07 19:05:22 +0200

    Graphs (So much fun!)

---
+++ Algomonster/Graphs/CountNumberOfIslands.cs
@@ -0,0 +1,77 @@
+﻿namespace ProjectDSA2.Algomonster.Graphs;
+
+public class CountNumberOfIslands
+{
+    public static IEnumerable<(int, int)> GetNeighbors((int row, int col) node, List<List<int>> grid)
+    {
+        List<int> rowDelta = new List<int>() { 0, -1, 0, 1 };
+        List<int> colDelta = new List<int>() { -1, 0, 1, 0 };
+
+        for (int i = 0; i < 4; i++)
+        {
+            int neighborRow = node.row + rowDelta[i];
+            int neighborCol = node.col + colDelta[i];
+            if (0 <= neighborRow && neighborRow < grid.Count && 
+                0 <= neighborCol && neighborCol < grid[0].Count &&
+                grid[neighborRow][neighborCol] == 1)
+            {
+                yield return (neighborRow, neighborCol);
+            }
+        }
+    }
+
+    public static void BFS(List<List<int>> grid, (int row, int col) node, HashSet<(int, int)> visited)
+    {
+        Queue<(int, int)> queue = new Queue<(int, int)> ();
+        queue.Enqueue(node);
+        visited.Add(node);
+
+        while (queue.Count > 0)
+        {
+            var dequeued = queue.Dequeue();
+            foreach (var neighbor in GetNeighbors(dequeued, grid))
+            {
+                if (visited.Contains(neighbor)) 
+                    continue;
+                queue.Enqueue(neighbor);
+                visited.Add(neighbor);
+            }
+        }
+    }
+
+    public static int CountIslands(List<List<int>> grid)
+    {
+        int numberOfIslands = 0;
+        HashSet<(int, int)> visited = new HashSet<(int, int)>();
+
+        for (int row = 0; row < grid.Count; row++)
+        {
+            for (int col = 0; col < grid[row].Count; col++)
+            {
+                if (!visited.Contains((row, col)) && grid[row][col] == 1)
+                {
+                    BFS(grid, (row, col), visited);
+                    numberOfIslands++;
+                }
+            }
+        }
+
+        return numberOfIslands;
+    }
+
+    public static void Driver()
+    {
+        int[][] grid2DArray = [
+            [1, 1, 1, 0, 0, 0],
+            [1, 1, 1, 1, 0, 0],
+            [1, 1, 1, 0, 0, 0],
+            [0, 1, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0],
+            [0, 0, 0, 0, 0, 0]
+        ];
+
+        List<List<int>> grid = grid2DArray.Select(x => x.ToList()).ToList();
+        int result = CountIslands(grid);
+        Console.WriteLine(result);
+    }
+}
---
+++ Algomonster/Graphs/GetKnightShortestPath.cs
@@ -0,0 +1,73 @@
+﻿namespace ProjectDSA2.Algomonster.Graphs;
+
+public class GetKnightShortestPath
+{
+    public static IEnumerable<(int, int)> GetNeighbors((int row, int col) square)
+    {
+        List<int> rowDeltas = new List<int>() 
+        { 
+            -1, -2, -2, -1, 1, 2, 2, 1
+        };
+        List<int> colDeltas = new List<int>()
+        {
+            -2, -1, 1, 2, 2, 1, -1, -2
+        };
+
+        for (int i = 0; i < 8; i++)
+        {
+            int neighborRow = square.row + rowDeltas[i];
+            int neighborCol = square.col + colDeltas[i];
+
+            yield return (neighborRow, neighborCol);
+        }
+    }
+
+    public static int BFS(int x, int y)
+    {
+        if (x == 0 && y == 0) return 0;
+
+        int result = 0;
+        HashSet<(int, int)> visited = new HashSet<(int, int)>();
+        Queue<(int, int)> queue = new Queue<(int, int)>();
+
+        visited.Add((0, 0));
+        queue.Enqueue((0, 0));
+
+        while (queue.Count > 0)
+        {
+            int n = queue.Count;
+            for (int i = 0; i < n; i++)
+            {
+                var (row, col) = queue.Dequeue();
+                if (row == x && col == y)
+                    return result;
+
+                foreach (var neighbor in GetNeighbors((row, col)))
+                {
+                    if (visited.Contains(neighbor))
+                        continue;
+                    visited.Add(neighbor);
+                    queue.Enqueue(neighbor);
+                }
+            }
+
+            result++;
+        }
+
+        return result;
+    }
+
+    public static int GetShortestPath(int x, int y)
+    {
+        return BFS(x, y);
+    }
+
+    public static void Driver()
+    {
+        int x = 5;
+        int y = 5;
+
+        int result = GetShortestPath(x, y);
+        Console.WriteLine(result);
+    }
+}
