88722e2372c7cac5f3182adc6be1828bb6fc5164
Ryan Harris <ryan@synthesis.co.za>
2025-11-12 13:36:23 +0200

    Graphs

---
+++ Algomonster/Graphs/SequenceReconstruction.cs
@@ -0,0 +1,93 @@
+﻿namespace ProjectDSA2.Algomonster.Graphs;
+
+public class SequenceReconstruction
+{
+    private static Dictionary<int, int> FindIndegree(Dictionary<int, List<int>> graph)
+    {
+        Dictionary<int, int> indegree = new Dictionary<int, int>();
+
+        foreach (var node in graph.Keys)
+        {
+            indegree[node] = 0;
+        }
+
+        foreach (var node in graph.Keys)
+        {
+            foreach (var neighbor in graph[node])
+            {
+                indegree[neighbor] = indegree.GetValueOrDefault(neighbor, 0) + 1;
+            }
+        }
+
+        return indegree;
+    }
+
+    private static Dictionary<int, List<int>> PopulateGraph(int n, List<List<int>> seqs)
+    {
+        Dictionary<int, List<int>> graph = new Dictionary<int, List<int>>();
+
+        for (int i = 0; i < n; i++)
+        {
+            graph.Add(i + 1, new List<int>()); // 1..6
+        }
+
+        foreach (List<int> seq in seqs)
+        {
+            for (int i = 0; i < seq.Count - 1; i++)
+            {
+                int first = seq[i];
+                int second = seq[i + 1];
+                graph[first].Add(second);
+            }
+        }
+
+        return graph;
+    }
+
+    public static bool Reconstruction(List<int> original, List<List<int>> seqs)
+    {
+        List<int> result = new List<int>();
+
+        Dictionary<int, List<int>> graph = PopulateGraph(original.Count, seqs);
+
+        Dictionary<int, int> indegree = FindIndegree(graph);
+
+        Queue<int> queue = new Queue<int>();
+
+        foreach (int node in indegree.Keys)
+        {
+            if (indegree[node] == 0)
+                queue.Enqueue(node);
+        }
+
+        while (queue.Count > 0)
+        {
+            if (queue.Count > 1)
+                return false;
+
+            int top = queue.Dequeue();
+            result.Add(top);
+
+            foreach (var neighbor in graph[top])
+            {
+                indegree[neighbor] -= 1;
+                if (indegree[neighbor] == 0)
+                    queue.Enqueue(neighbor);
+            }
+        }
+
+        return result.SequenceEqual(original);
+    }
+
+    public static void Driver()
+    {
+        List<int> original = new List<int>() { 4, 1, 5, 2, 6, 3 };
+        List<List<int>> seqs = new List<List<int>>()
+        {
+            new() { 5, 2, 6, 3  },
+            new() { 4, 1, 5, 2  },
+        };
+        bool result = Reconstruction(original, seqs);
+        Console.WriteLine(result);
+    }
+}
---
+++ Algomonster/Graphs/TaskScheduling.cs
@@ -0,0 +1,88 @@
+﻿using System.Threading.Tasks;
+
+namespace ProjectDSA2.Algomonster.Graphs;
+
+public class TaskScheduling
+{
+    private static Dictionary<string, int> FindIndegree(Dictionary<string, List<string>> graph)
+    {
+        Dictionary<string, int> indegree = new Dictionary<string, int>();
+
+        foreach (var node in graph.Keys)
+        {
+            indegree.Add(node, 0);
+        }
+
+        foreach (var node in graph.Keys)
+        {
+            foreach (var neighbor in graph[node])
+            {
+                indegree[neighbor] = indegree.GetValueOrDefault(neighbor, 0) + 1;
+            }
+        }
+
+        return indegree;
+    }
+
+    private static Dictionary<string, List<string>> PopulateGraph(List<string> tasks, List<List<string>> requirements)
+    {
+        Dictionary<string, List<string>> graph = new Dictionary<string, List<string>>();
+        foreach (string task in tasks)
+        {
+            graph.Add(task, new List<string>());
+        }
+
+        foreach (var requirement in requirements)
+        {
+            string first = requirement[0];
+            string second = requirement[1];
+
+            graph[first].Add(second);
+        }
+        return graph;
+    }
+
+    public static List<string> Scheduling(List<string> tasks, List<List<string>> requirements)
+    {
+        List<string> result = new List<string>();
+
+        Dictionary<string, List<string>> graph = PopulateGraph(tasks, requirements);
+        Queue<string> queue = new Queue<string>();
+
+        var indegree = FindIndegree(graph);
+
+        foreach (var node in indegree.Keys)
+        {
+            if (indegree[node] == 0)
+                queue.Enqueue(node);
+        }
+
+        while (queue.Count > 0)
+        {
+            var node = queue.Dequeue();
+            result.Add(node);
+
+            foreach (var neighbor in graph[node])
+            {
+                indegree[neighbor] -= 1;
+                if (indegree[neighbor] == 0)
+                    queue.Enqueue(neighbor);
+            }
+        }        
+
+        return result;
+    }
+
+    public static void Driver()
+    {
+        List<string> tasks = new List<string>() { "a", "b", "c", "d" };
+        List<List<string>> requirements = new List<List<string>>()
+        {
+            new() { "a", "b" },
+            new() { "c", "b" },
+            new() { "b", "d" },
+        };
+        List<string> result = Scheduling(tasks, requirements);
+        Console.WriteLine(string.Join(", ", result));
+    }
+}
