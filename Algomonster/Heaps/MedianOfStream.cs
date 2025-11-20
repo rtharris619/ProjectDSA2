7fab3329ae459d84a3b101a8925fbc985ad2ceb8
Ryan Harris <ryan@synthesis.co.za>
2025-11-20 09:41:14 +0200

    Heaps

---
+++ Algomonster/Heaps/MedianOfStream.cs
@@ -0,0 +1,53 @@
+﻿namespace ProjectDSA2.Algomonster.Heaps;
+
+public class MedianOfStream
+{
+    List<int> stream = new List<int>();
+
+    public void AddNum(int num)
+    {
+        stream.Add(num);
+        stream.Sort(); // O(nlogn)
+    }
+
+    public double GetMean()
+    {
+        double mean = 0.0;
+        double sum = 0;
+
+        foreach (int num in stream)
+        {
+            sum += num;
+        }
+
+        mean = sum / stream.Count;
+        mean = Math.Round(mean, 1);
+
+        return mean;
+    }
+
+    public double GetMedian() 
+    {
+        double median = 0.0;
+
+        if (stream.Count % 2 == 0) // evens
+            median = (stream[(stream.Count / 2)] + stream[(stream.Count / 2) - 1]) / 2.0;
+        else // odds
+            median = stream[stream.Count / 2];
+
+        return median;
+    }
+
+    public static void Driver()
+    {
+        var stream = new MedianOfStream();
+        stream.AddNum(1);
+        stream.AddNum(2);
+        stream.AddNum(3);
+        var result = stream.GetMedian();
+        Console.WriteLine(result);
+        stream.AddNum(4);
+        result = stream.GetMedian();
+        Console.WriteLine(result);
+    }
+}
---
+++ Algomonster/Heaps/MedianOfStream2.cs
@@ -0,0 +1,56 @@
+﻿namespace ProjectDSA2.Algomonster.Heaps;
+
+public class MedianOfStream2
+{
+    PriorityQueue<int, int> maxHeap = new PriorityQueue<int, int>(Comparer<int>.Create((a, b) => b.CompareTo(a)));
+    PriorityQueue<int, int> minHeap = new PriorityQueue<int, int>();
+
+    public void AddNum(int num)
+    {
+        if (minHeap.Count == 0 || num < minHeap.Peek())
+        {
+            maxHeap.Enqueue(num, num);
+        }
+        else
+        {
+            minHeap.Enqueue(num, num);
+        }
+        Balance();
+    }
+
+    public double GetMedian() 
+    {
+        if (maxHeap.Count == minHeap.Count)
+        {
+            return (maxHeap.Peek() + minHeap.Peek()) / 2.0;
+        }
+        return maxHeap.Peek();
+    }
+
+    public void Balance()
+    {
+        if (maxHeap.Count < minHeap.Count)
+        {
+            int val = minHeap.Dequeue();
+            maxHeap.Enqueue(val, val);
+        }
+        if (maxHeap.Count > minHeap.Count + 1)
+        {
+            int val = maxHeap.Dequeue();
+            minHeap.Enqueue(val, val);
+        }
+    }
+
+    public static void Driver()
+    {
+        var stream = new MedianOfStream();
+        stream.AddNum(1);
+        stream.AddNum(2);
+        stream.AddNum(3);
+        var result = stream.GetMedian();
+        Console.WriteLine(result);
+        stream.AddNum(4);
+        result = stream.GetMedian();
+        Console.WriteLine(result);
+    }
+}
