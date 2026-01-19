f7d2544febef005e332d7b011978a4aa7d0c7689
Ryan Harris <ryan@synthesis.co.za>
2026-01-19 15:29:14 +0200

    DP challenges.

---
+++ Algomonster/DynamicProgramming/MinPathSum.cs
@@ -0,0 +1,85 @@
+﻿using System.Runtime.Serialization;
+
+namespace ProjectDSA2.Algomonster.DynamicProgramming;
+
+public class MinPathSum
+{
+
+    public static int MinPath(List<List<int>> grid)
+    {
+        int rows = grid.Count;
+        int cols = grid[0].Count;
+
+        int[,] dp = new int[rows, cols];
+        dp[0, 0] = grid[0][0];
+        for (int row = 1; row < rows; row++)
+        {
+            dp[row, 0] = dp[row - 1, 0] + grid[row][0];
+        }
+        for (int col = 1; col < cols; col++)
+        {
+            dp[0, col] = dp[0, col - 1] + grid[0][col];
+        }
+
+        for (int row = 1; row < rows; row++)
+        {
+            for (int col = 1; col < cols; col++)
+            {
+                dp[row, col] = Math.Min(dp[row - 1, col], dp[row, col - 1]) + grid[row][col];
+            }
+        }
+
+        return dp[rows - 1, cols - 1];
+    }
+
+    /// <summary>
+    /// Space Optimised Solution
+    /// </summary>
+    /// <param name="grid"></param>
+    /// <returns></returns>
+    public static int MinPath2(List<List<int>> grid)
+    {
+        int rows = grid.Count;
+        int cols = grid[0].Count;
+        int[] dp = new int[cols];
+
+        for (int row = 0; row < rows; row++)
+        {
+            for (int col = 0; col < cols; col++)
+            {
+                var cell = grid[row][col];
+                if (row == 0 && col == 0) // first row and column.
+                {
+                    dp[0] = cell;
+                }
+                else if (row == 0) // first row, columns other than first column.
+                {
+                    dp[col] = dp[col - 1] + cell;
+                }
+                else if (col == 0) // rows other than first row.
+                {
+                    dp[col] = dp[col] + cell;
+                }
+                else // middle rows and columns.
+                {
+                    dp[col] = Math.Min(dp[col - 1], dp[col]) + cell;
+                }
+            }
+        }
+
+        return dp[^1];
+    }
+
+    public static void Driver()
+    {
+        var grid = new List<List<int>>
+        {
+            new() { 1, 3, 1 },
+            new() { 1, 5, 1 },
+            new() { 4, 2, 1 },
+        };
+
+        var result = MinPath2(grid);
+        Console.WriteLine(result);
+    }
+}
---
+++ Algomonster/DynamicProgramming/UniquePathsWithObstacles.cs
@@ -0,0 +1,74 @@
+﻿namespace ProjectDSA2.Algomonster.DynamicProgramming;
+
+public class UniquePathsWithObstacles
+{
+    public static int UniquePathsIi(List<List<int>> obstacleGrid)
+    {
+        int n = obstacleGrid.Count;
+        int m = obstacleGrid[0].Count;
+        int[,] dp = new int[n, m];
+
+        if (obstacleGrid[0][0] == 1) return 0;
+        
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                if (obstacleGrid[i][j] == 1)
+                {
+                    dp[i, j] = 0;
+                }
+                else if (i == 0 || j == 0)
+                {
+                    dp[i, j] = 1;
+                }
+                else
+                {
+                    dp[i, j] = dp[i - 1, j] + dp[i, j - 1];
+                }
+            }
+        }
+
+        return dp[n - 1, m - 1];
+    }
+
+    public static int UniquePathsIi2(List<List<int>> obstacleGrid)
+    {
+        if (obstacleGrid[0][0] == 1) return 0;
+
+        int rows = obstacleGrid.Count;
+        int cols = obstacleGrid[0].Count;
+
+        int[] dp = new int[cols];
+        dp[0] = 1;
+
+        for (int row = 0; row < rows; row++)
+        {
+            for (int col = 0; col < cols; col++)
+            {
+                if (obstacleGrid[row][col] == 1)
+                {
+                    dp[col] = 0;
+                }
+                else if (col > 0)
+                {
+                    dp[col] += dp[col - 1];
+                }
+            }
+        }
+
+        return dp[^1];
+    }
+
+    public static void Driver()
+    {
+        var obstacleGrid = new List<List<int>>
+        {
+            new() { 0, 0, 0 },
+            new() { 0, 1, 0 },
+            new() { 0, 0, 0 },
+        };
+        var result = UniquePathsIi2(obstacleGrid);
+        Console.WriteLine(result);
+    }
+}
