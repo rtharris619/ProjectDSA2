dcec5c5149c7d1c87007af1f4f59fe18c7dc22be
Ryan Harris <ryan@synthesis.co.za>
2026-01-22 17:13:31 +0200

    DP challenges

---
+++ Algomonster/DynamicProgramming/DungeonGame.cs
@@ -0,0 +1,50 @@
+﻿namespace ProjectDSA2.Algomonster.DynamicProgramming;
+
+public class DungeonGame
+{
+
+    public static int Game(List<List<int>> dungeon)
+    {
+        int rows = dungeon.Count;
+        int cols = dungeon[0].Count;
+
+        int[,] dp = new int[rows + 1, cols + 1];
+
+        for (int row = rows - 1; row >= 0; row--)
+        {
+            for (int col = cols - 1; col >= 0; col--)
+            {
+                if (row == rows - 1 && col == cols - 1) // last cell (Princess)
+                {
+                    dp[row, col] = Math.Max(1 - dungeon[row][col], 1);
+                }
+                else if (row == rows - 1) // last row
+                {
+                    dp[row, col] = Math.Max(dp[row, col + 1] - dungeon[row][col], 1);
+                }
+                else if (col == cols - 1) // last col
+                {
+                    dp[row, col] = Math.Max(dp[row + 1, col] - dungeon[row][col], 1);
+                }
+                else
+                {
+                    dp[row, col] = Math.Max(Math.Min(dp[row, col + 1], dp[row + 1, col]) - dungeon[row][col], 1);
+                }
+            }
+        }
+        
+        return dp[0,0];
+    }
+
+    public static void Driver()
+    {
+        var dungeon = new List<List<int>>
+        {
+            new() { -2,  -3,  3 },
+            new() { -5, -10,  1 },
+            new() { 10,  30, -5 },
+        };
+        var result = Game(dungeon);
+        Console.WriteLine(result);
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,4 +2,4 @@
 using ProjectDSA2.Algomonster.DynamicProgramming;
 using ProjectDSA2.Algomonster.TwoPointers;
 
-Triangle.Driver();
+DungeonGame.Driver();
